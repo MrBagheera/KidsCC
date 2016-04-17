8778b8a25f6e86d8eb1289544112844fa011b62e
Dmitry Mikhaylov <mikhailov.dmitry@gmail.com>
2016-04-17 19:00:39 +0300

    first draft

---
+++ Assets/Scripts/ElevatorView.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using JetBrains.Annotations;
+using UnityEngine.UI;
+
+public class ElevatorView : MonoBehaviour
+{
+
+    public Button UpButton;
+    public Button DownButton;
+    public AudioSource AudioSource;
+    public AudioClip EvevatorGoesUpClip;
+    public AudioClip EvevatorGoesDownClip;
+
+    [UsedImplicitly]
+    void OnEnable() {
+	    UpButton.onClick.AddListener(() =>
+	    {
+	        Debug.Log("Up");
+            AudioSource.PlayOneShot(EvevatorGoesUpClip);
+	    });
+        DownButton.onClick.AddListener(() =>
+        {
+            Debug.Log("Down");
+            AudioSource.PlayOneShot(EvevatorGoesDownClip);
+        });
+    }
+
+    [UsedImplicitly]
+    void OnDisable()
+    {
+        UpButton.onClick.RemoveAllListeners();
+        DownButton.onClick.RemoveAllListeners();
+    }
+}
